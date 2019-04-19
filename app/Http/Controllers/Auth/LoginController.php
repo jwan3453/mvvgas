70f9a2ef288ba274b489c1784498869066eaec9c
jackywang <jackyloop@outlook.com>
2019-04-19 14:54:54 +0800

    add middeware for corss origin

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -69,7 +69,6 @@ class LoginController extends Controller
 		if ($this->attemptLogin($request)) {
 			$user = $this->guard()->user();
 			$user->generateToken();
-			
 			return response()->json([
 				'response' => $user->toArray(),
 			]);
